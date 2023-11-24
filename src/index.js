902f1f7c1ab75deaf81401f3156d4edbb29cdf3a
Semen Evseev <117739297+Semochkaelse@users.noreply.github.com>
2023-11-24 07:44:27 +0200

    PT-10502 add init blbabla major

--- src/index.js
+++ src/index.js
@@ -1,59 +1,49 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
+const path = require('path');
+
+const packageJson = require('../package.json');
 
 const generateChangelog = () => {
-  const rawCommits = execSync('git log --pretty=format:"%s %ad %h" --date=short').toString().split('\n');
-  console.log(rawCommits);
-  let changelog = "# Changelog\n\n";
-  let currentVersion = "";
-  let versionContents = {
-    enhancements: [],
-    fixes: [],
-    closedIssues: [],
-    mergedPRs: []
-  };
+  const rawCommits = execSync('git log --pretty=format:"%s"').toString().split('\n');
+  let changelogPath = path.join(__dirname, 'CHANGELOG.md');
+  let changelogContent = fs.existsSync(changelogPath) ? fs.readFileSync(changelogPath).toString() : '';
 
-  const addToVersionContent = (type, message) => {
-    if (!versionContents[type].includes(message)) {
-      versionContents[type].push(message);
-    }
-  };
+  let currentVersion = packageJson.version.split('.').map(Number);
+  let versionString = `v${currentVersion.join('.')}`;
+  let versionHeader = `## ${versionString}\n\n`;
 
-  rawCommits.forEach(commit => {
-    const [hash, message, date] = commit.split(' ');
-    if (message.startsWith('v')) {
-      if (currentVersion) {
-        changelog += formatVersionContent(currentVersion, versionContents);
-        versionContents = { enhancements: [], fixes: [], closedIssues: [], mergedPRs: [] };
-      }
-      currentVersion = message + " (" + date + ")";
-    } else if (message.startsWith('feat')) {
-      addToVersionContent('enhancements', message);
-    } else if (message.startsWith('fix')) {
-      addToVersionContent('fixes', message);
-    } 
-  });
-
-  if (currentVersion) {
-    changelog += formatVersionContent(currentVersion, versionContents);
+  if (!changelogContent.includes(versionHeader)) {
+    changelogContent = versionHeader + changelogContent;
   }
 
-  fs.writeFileSync('CHANGELOG.md', changelog);
-};
+  rawCommits.forEach(commit => {
+    let [issueNumber, message] = commit.split(' ', 2);
+    let commitType = message.split(' ').pop();
+    let jiraLink = `https://jira.action-media.ru/browse/${issueNumber}`;
 
-const formatVersionContent = (version, contents) => {
-  let formatted = `## ${version}\n`;
-  formatted += "Full Changelog\n\n";
-  formatted += formatSection("Implemented enhancements:", contents.enhancements);
-  formatted += formatSection("Fixed bugs:", contents.fixes);
-  formatted += formatSection("Closed issues:", contents.closedIssues);
-  formatted += formatSection("Merged pull requests:", contents.mergedPRs);
-  return formatted;
-};
+    if (['major', 'minor', 'patch'].includes(commitType)) {
+      let index = ['major', 'minor', 'patch'].indexOf(commitType);
+      currentVersion[index]++;
+      for (let i = index + 1; i < currentVersion.length; i++) {
+        currentVersion[i] = 0;
+      }
+      versionString = `v${currentVersion.join('.')}`;
+      versionHeader = `## ${versionString}\n\n`;
 
-const formatSection = (title, items) => {
-  if (items.length === 0) return '';
-  return `${title}\n${items.map(item => `- ${item}`).join('\n')}\n\n`;
+      if (!changelogContent.includes(versionHeader)) {
+        changelogContent = versionHeader + changelogContent;
+      }
+    }
+
+    if (changelogContent.includes(versionHeader)) {
+      let insertPoint = changelogContent.indexOf(versionHeader) + versionHeader.length;
+      let commitEntry = `- ${message} ([${issueNumber}](${jiraLink}))\n`;
+      changelogContent = changelogContent.slice(0, insertPoint) + commitEntry + changelogContent.slice(insertPoint);
+    }
+  });
+
+  fs.writeFileSync(changelogPath, changelogContent);
 };
 
 generateChangelog();
